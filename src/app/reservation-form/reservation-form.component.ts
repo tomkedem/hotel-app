7e31ebe927575036a26f5cb867dbc6ad05bb6053
Tomer Kedem <tompha1@hotmail.com>
2023-12-21 08:05:34 +0200

    Injecting the reservation service into the form component

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
@@ -9,7 +11,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({})
   
-  constructor(private formBuilder: FormBuilder){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService){
+
+    }
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
@@ -22,10 +28,9 @@ export class ReservationFormComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.reservationForm.status);
-
     if (this.reservationForm.valid) {
-      console.log("valid");
+      let reservation: Reservation = this.reservationForm.value
+      this.reservationService.addReservation(reservation)
 
     }
   }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -18,6 +18,8 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation)
+    console.log(this.reservations);
+    
   }
   deleteReservation(id: string): void{
     let index = this.reservations.findIndex(res => res.id === id)
