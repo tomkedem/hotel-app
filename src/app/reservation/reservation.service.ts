df332cdc89ce6d47e6e840b86367d6670c5aa799
Tomer Kedem <tompha1@hotmail.com>
2023-12-21 18:30:11 +0200

    Creating unique ids for reservations

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -26,6 +26,7 @@ export class ReservationService {
   }
 
   addReservation(reservation: Reservation): void {
+    reservation.id = Date.now().toString()
     this.reservations.push(reservation)
     localStorage.setItem("reservations",JSON.stringify(this.reservations))       
   }
